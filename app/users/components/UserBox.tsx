961a281d7b3bbd3b0147ff76ba170bd4f50bfc29
Andrew <andrew.suponev@gmail.com>
2023-06-02 11:08:06 +0400

    feat: implement loading states

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from 'react'
 import axios from 'axios'
 
 import Avatar from '@/app/components/Avatar'
+import LoadingModal from '@/app/components/LoadingModal'
 
 interface IUserBoxProps {
   data: User
@@ -30,21 +31,26 @@ const UserBox: React.FC<IUserBoxProps> = ({
   }, [data, router])
 
   return (
-    <div
-      onClick={handleClick}
-      className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
-    >
-      <Avatar user={data} />
-      <div className='min-w-0 flex-1'>
-        <div className='focus:outline-none'>
-          <div className='flex justify-between items-center mb-1'>
-            <p className='text-sm font-medium text-gray-900'>
-              {data.name}
-            </p>
+    <>
+      {isLoading && (
+        <LoadingModal />
+      )}
+      <div
+        onClick={handleClick}
+        className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
+      >
+        <Avatar user={data} />
+        <div className='min-w-0 flex-1'>
+          <div className='focus:outline-none'>
+            <div className='flex justify-between items-center mb-1'>
+              <p className='text-sm font-medium text-gray-900'>
+                {data.name}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
